f02a05881c602a8b08ffe1b9ac90ecd13db4e5aa
twjin03 <127813291+twjin03@users.noreply.github.com>
2024-08-06 01:48:44 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 160 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C11/백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.c
@@ -0,0 +1,61 @@
+//수정사항: for문 loop 범위에서 오류 남
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <limits.h>
+
+int main() {
+    int N, K;
+    scanf("%d %d", &N, &K);
+
+    int* arr = malloc(sizeof(int) * N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int temp = 0;
+    int count = 0; 
+    int max = 0;
+    
+
+    for (int i = N-1; i >= 0; i--) {
+        int maxIdx = i;
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[maxIdx]) {
+                maxIdx = j;
+            }
+        }
+
+        
+
+        if (maxIdx != i) {
+            temp = arr[i];
+            
+            arr[i] = arr[maxIdx];
+            max = arr[maxIdx];
+            arr[maxIdx] = temp;
+
+            
+            count++;
+        }
+
+        if (count == K)
+            break;
+
+        
+    }
+
+    if (count < K) {
+        printf("%d", -1);
+    }
+
+    else {
+        printf("%d %d", temp, max);
+    }
+
+    free(arr);
+
+    return 0;
+}
