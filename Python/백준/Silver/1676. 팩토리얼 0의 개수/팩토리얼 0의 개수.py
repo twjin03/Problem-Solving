56353d523fbd292dc7839ff465a53f653cdf557d
twjin03 <127813291+twjin03@users.noreply.github.com>
2025-02-12 06:15:05 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -0,0 +1,19 @@
+
+
+n = int(input())
+result = 1
+
+for i in range(1, n+1):
+    result *= i
+
+
+
+cnt = 0
+while True:
+    if result % 10 == 0:
+        cnt += 1
+        result //= 10  ### 런타임에러 방지 
+    else:
+        break
+
+print(cnt)
